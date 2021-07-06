9ee9a0eac54ddda7c188bfcf7c66a8f1ac57b375
a4c5v2b <andrewcheng2016@gmail.com>
2021-07-07 02:40:18 +0800

    Week 4 (Function)

---
+++ Week 4/demonstrate loop with while/demonstrate loop with while/Source.cpp
@@ -0,0 +1,37 @@
+/* C for Everyone: Programming Fundamentals
+	Week 4: Function code example */
+
+#include <stdio.h>
+
+
+void wrt_very(int count)
+{
+	while (count > 0)
+	{
+		printf("\n very");
+		count--;
+
+	};
+	printf(" much.\n\n");
+
+
+}
+
+
+
+
+int main(void)
+{
+
+	int repeat = 0;
+	printf("How strong is your love 1-10? : ");
+	scanf_s("%d", &repeat);
+	printf("\nI love you very");
+	wrt_very(repeat);
+	return 0;
+
+
+
+}
+
+
