b6f6897dc39b5b82b4369330311b90b90a295f31
a4c5v2b <andrewcheng2016@gmail.com>
2021-07-17 01:54:53 +0800

    Course 2_Week 2
    Assignment will be done later

---
+++ Course 2/Week 2/Week 2 Quiz/Week 2 Quiz/Source.cpp
@@ -0,0 +1,36 @@
+/* C for Everyone: Structured Programming
+	Week 1: Week 2 Quiz */
+
+
+#include <stdio.h>
+
+
+int main()
+{
+
+	// Q4 = 4.000000 
+	/*
+	struct point {
+		double x;
+		double y;
+	} p1 = { 4, 4.0 };
+
+	printf("%f\n", p1.x);
+	*/
+
+	// Q6 = 0
+
+	
+
+	struct point {
+		double x;
+		double y;
+	} p1;
+
+
+	struct point* ptr = &p1;
+	
+
+	printf("%f", ptr->y);
+
+}
